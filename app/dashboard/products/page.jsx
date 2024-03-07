07a03c30a1a659800907cadd385530f8de237bda
ferifer <ferihandoyo04@gmail.com>
2024-03-07 20:32:29 +0700

    products and users

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,7 +1,68 @@
-const ProductsPage = () =>{
-    return(
-        <div>ProductsPage</div>
-    )
-}
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import styles from "@/app/ui/dashboard/products/products.module.css";
+import Search from "@/app/ui/dashboard/search/search";
+import Image from "next/image";
+import Link from "next/link";
+const ProductsPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a product..." />
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.product}>
+                <Image
+                  src="/noproduct.jpg"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.productImage}
+                />
+                Mie Goreng
+              </div>
+            </td>
+            <td>Description1</td>
+            <td>Rp. 100.000</td>
+            <td>13.04.2023</td>
+            <td>1.000</td>
 
-export default ProductsPage
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
+                  </button>
+                </Link>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
+};
+
+export default ProductsPage;
